4878519d779ec51f1422094873152c9049be17b9
Sallem Ahmed <sallem@aptoslabs.com>
2024-02-01 11:43:27 -0500

    update exports

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import IndexerProvider, {useIndexerContext} from "./src/providers/IndexerProvide
 import NetworkProvider, {useNetworkContext} from "./src/providers/NetworkProvider";
 import TransactionProvider, {useTransactionContext} from "./src/providers/TransactionProvider";
 import PayloadProvider, {usePayloadContext} from "./src/providers/PayloadProvider";
-import { lookUpImage } from "./src/utils/checkImage";
+import { lookUpImage, blobToBase64, fileToUint8array, uint8arrayToBase64 } from "./src/utils/checkImage";
 import * as Constants from "./src/utils/constants"
 
 export {
@@ -14,6 +14,6 @@ export {
     useNetworkContext,
     useTransactionContext,
     usePayloadContext,
-    lookUpImage,
+    lookUpImage, blobToBase64, fileToUint8array, uint8arrayToBase64,
     Constants
 };
